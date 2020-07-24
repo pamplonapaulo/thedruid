656dfe96d7faf59fce564e20d82d863c5f3ddc00
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-07-24 18:53:05 -0300

    media query padding top

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -5,13 +5,17 @@ export const Wrapper = styled.main`
   color: #056839;
   width: 100%;
   height: 100%;
-  padding: 18rem 4rem 4rem;
+  padding: 15rem 4rem 4rem;
   text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   overflow: auto;
+
+  @media only screen and (min-width: 1024px) {
+    padding-top: 8rem;
+  }
 `
 
 export const Logo = styled.img`
